fde05fc03f4ae44ddebb4364365ef7168a35de14
Owen Campbell <owen.campbell@tanti.org.uk>
2022-05-26 14:05:38 +0100

    Fix licence

--- amoni/__init__.py
+++ amoni/__init__.py
@@ -1,3 +1,10 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021 The Amoni project team members listed at
+# https://github.com/anvilistas/amoni/graphs/contributors
+#
+# This software is published at https://github.com/anvilistas/amoni
+
 """A tool to manage local development of Anvil applications"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- amoni/cli.py
+++ amoni/cli.py
@@ -8,7 +8,7 @@ import typer
 
 from . import api
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 amoni = typer.Typer()
 
