e912cb094c89c26509d77ba0deee431ebdec081f
Owen Campbell <owen.campbell@tanti.org.uk>
2022-06-10 12:39:22 +0100

    Tidy up printing and docstrings

--- amoni/cli/amoni.py
+++ amoni/cli/amoni.py
@@ -4,6 +4,7 @@
 # https://github.com/anvilistas/amoni/graphs/contributors
 #
 # This software is published at https://github.com/anvilistas/amoni
+from functools import partial
 from pathlib import Path
 
 import typer
@@ -15,7 +16,12 @@ from . import install
 __version__ = "0.0.5"
 
 cmd = typer.Typer()
-cmd.add_typer(install.cmd, name="install")
+cmd.add_typer(install.cmd, name="install", help="Install an anvil app or dependency")
+
+_error = partial(typer.secho, fg=typer.colors.RED, err=True)
+_success = partial(typer.secho, fg=typer.colors.GREEN)
+_progress = partial(typer.secho, fg=typer.colors.CYAN)
+_done = partial(_success, "Done! ✨️")
 
 
 @cmd.callback()
@@ -30,29 +36,33 @@ def init(
     ),
     app: str = typer.Argument("hello_world", help="App Folder Name"),
 ):
+    """Create the amoni folder structure and initialise a git repo there"""
     try:
         api.init(directory, app)
-        typer.echo(f"Amoni project created in {directory}")
-        typer.secho("Done! ✨️", fg=typer.colors.GREEN)
+        _progress(f"Amoni project created in {directory}")
+        _done()
     except OutputDirExistsException:
-        red = typer.colors.RED
-        typer.secho("⚠️ Error creating project:", fg=red)
-        typer.secho(f"{directory} already exists", fg=red)
+        _error("Error creating project:")
+        _error(f"{directory} already exists")
 
 
 @cmd.command()
 def start(
-    update: bool = typer.Option(False, help="Whether to update the docker images")
+    update: bool = typer.Option(False, help="Whether to update the docker images"),
+    launch: bool = typer.Option(True, help="Whether to launch the app"),
 ):
     """Start the anvil app and db servers"""
     if update:
         typer.echo("Rebuilding server images")
         api.build_image("app")
         api.pull_image("db")
-    typer.echo("Starting anvil app and database servers")
+    _progress("Starting anvil app and database servers")
     api.start_service("app", detach=True)
-    typer.echo("Your app is available at http://localhost:3030")
-    typer.secho("Done! ✨️", fg=typer.colors.GREEN)
+    _progress("Your app is available at http://localhost:3030")
+    if launch:
+        _progress("Launching app")
+        typer.launch("http://localhost:3030")
+    _done()
 
 
 @cmd.command()
